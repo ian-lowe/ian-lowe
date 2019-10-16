69d67afc33b0eaf9047bf08b3f883ae8205e638a
ian-lowe <ilowe@mac.com>
2019-10-16 16:22:52 -0700

    clean up

--- squares.js
+++ squares.js
@@ -1,13 +1,7 @@
 var squares = document.getElementsByClassName("square");
 
-
 for(var i = 0; i < squares.length; i++) {
 	squares[i].addEventListener("mouseover", function(){
 		this.style.backgroundColor = "pink";
 	});
 }
-
-// function pickColor(){
-// 	 var n = Math.floor(Math.random() * colors.length);
-// 	return n;
-// }
